87f43cdbdf996eb2717c2a4e5bb811ad39cfd79e
GitHub Action <action@github.com>
2020-12-12 14:50:18 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/erkebek/intellijideaplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.erkebek.intellijideaplugintemplate.services
+
+import com.github.erkebek.intellijideaplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/erkebek/intellijideaplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/erkebek/intellijideaplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.erkebek.intellijideaplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.erkebek.intellijideaplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
